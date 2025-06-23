b1666b42a11844afcf79206d35041ea013820377
Akingram <guilland.killian@gmail.com>
2025-06-23 14:25:38 +0200

    Basic sqlite3 setup

---
+++ model.py
@@ -0,0 +1,15 @@
+from flask import g
+import sqlite3
+
+DATABASE = 'db/database.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
