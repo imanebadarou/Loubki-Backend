8141e6c6d4f5c963324b3d4e8c8f6e1040dce70c
Akingram <guilland.killian@gmail.com>
2025-06-23 15:34:57 +0200

    Added hello world endpoint with flask-restx

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template
 from model import close_connection, list_histoires
+from flask_restx import Api, Resource
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
-def home():
-    """Render the home page."""
-    histoires = list_histoires()
-    return render_template('home.html', histoires=histoires)
+@api.route('/hello')
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 @app.teardown_appcontext
 def app_teardown(exception):
