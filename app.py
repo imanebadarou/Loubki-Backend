c92a0dbbe40e0f0da2cc691ebd5babb4e72556e3
imanebadarou <54185750+imanebadarou@users.noreply.github.com>
2025-06-23 17:01:12 +0200

    methodes sql

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from model import close_connection, list_histoires
+from model import *
 from flask_restx import Api, Resource
 
 app = Flask(__name__)
@@ -17,3 +17,33 @@ if __name__ == '__main__':
 def app_teardown(exception):
     """Close the database connection at the end of the request."""
     close_connection(exception)
+
+@api.route('/histoires')
+class Histoire(Resource):
+    def get(self):
+        histoires = list_histoires()
+        return histoires
+    
+@api.route('/histoire/<key>', methods=["GET"])
+class Histoire(Resource):
+    def get(self, key):
+        histoireid = histoire_id(key)
+        return histoireid
+
+@api.route('/histoire/<key>/choix', methods=["GET"])
+class Histoire(Resource):
+    def get(self, key):
+        histoireid = histoire_choix(key)
+        return histoireid
+
+@api.route('/choix')
+class Choix(Resource):
+    def get(self):
+        choix = list_choix()
+        return choix
+    
+@api.route('/choix/<key>', methods=["GET"])
+class Choix(Resource):
+    def get(self, key):
+        choixid = choix_id(key)
+        return choixid
--- model.py
+++ model.py
@@ -17,7 +17,37 @@ def list_histoires():
     rows = cur.fetchall()
     return [dict(zip(columns, row)) for row in rows]
 
+def histoire_id(id):
+    cur = get_db().cursor()
+    cur.execute('SELECT * FROM histoire WHERE id_hist=?',(id,))
+    columns = [desc[0] for desc in cur.description]
+    rows = cur.fetchall()
+    return [dict(zip(columns, row)) for row in rows]
+
+def histoire_choix(id):
+    cur = get_db().cursor()
+    cur.execute('SELECT id_choix, txt_choix, chap_prev, chap_next FROM choix JOIN chapitre ON choix.chap_prev = chapitre.id_chap WHERE chapitre.id_hist=?',(id,))
+    columns = [desc[0] for desc in cur.description]
+    rows = cur.fetchall()
+    return [dict(zip(columns, row)) for row in rows]
+
+def list_choix():
+    cur = get_db().cursor()
+    cur.execute("SELECT * FROM choix")
+    columns = [desc[0] for desc in cur.description]
+    rows = cur.fetchall()
+    return [dict(zip(columns, row)) for row in rows]
+
+def choix_id(id):
+    cur = get_db().cursor()
+    cur.execute('SELECT * FROM choix WHERE id_hist=?',(id,))
+    columns = [desc[0] for desc in cur.description]
+    rows = cur.fetchall()
+    return [dict(zip(columns, row)) for row in rows]
+
+
 def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
+
