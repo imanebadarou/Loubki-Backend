537c0f3ec8923bb23397c8fd9eadac2ad371a0a6
Akingram <guilland.killian@gmail.com>
2025-06-24 18:04:48 +0200

    Added missing model functions

--- model/choice.py
+++ model/choice.py
@@ -3,7 +3,7 @@ from db import get_db
 @staticmethod
 def list_choices():
     cur = get_db().cursor()
-    cur.execute("SELECT * FROM choices")
+    cur.execute("SELECT * FROM choice")
     columns = [desc[0] for desc in cur.description]
     rows = cur.fetchall()
     return [dict(zip(columns, row)) for row in rows]
@@ -12,6 +12,14 @@ def list_choices():
 def get_choice(id):
     cur = get_db().cursor()
     cur.execute('SELECT * FROM choice WHERE id_choix=?', (id,))
+    columns = [desc[0] for desc in cur.description]
+    rows = cur.fetchall()
+    return [dict(zip(columns, row)) for row in rows]
+
+@staticmethod
+def list_chapter_choices(chapter_id):
+    cur = get_db().cursor()
+    cur.execute('SELECT * FROM choice JOIN chapter ON choice.prev_chapter_id = chapter.id WHERE story_id=? AND chapter.id=?', (chapter_id))
     columns = [desc[0] for desc in cur.description]
     rows = cur.fetchall()
     return [dict(zip(columns, row)) for row in rows]
--- model/item.py
+++ model/item.py
@@ -8,34 +8,18 @@ def list_items():
     rows = cur.fetchall()
     return [dict(zip(columns, row)) for row in rows]
 
-# @staticmethod
-# def obj_requis():
-#     cur = get_db().cursor()
-#     cur.execute("SELECT O.id_obj, O.nom_obj, R.quantite, R.perd_obj, C.id_choix, C.txt_choix FROM objet O JOIN requis R ON O.id_obj=R.id_obj JOIN choix C ON C.id_choix=R.id_choix")
-#     columns = [desc[0] for desc in cur.description]
-#     rows = cur.fetchall()
-#     return [dict(zip(columns, row)) for row in rows]
+@staticmethod
+def list_required_items(choice_id):
+    cur = get_db().cursor()
+    cur.execute("SELECT item.id, item.label, required.quantity, required.lose_item FROM item JOIN required ON item.id=required.item_id JOIN choice ON choice.id=required.choice_id WHERE choice.id=?", (choice_id,))
+    columns = [desc[0] for desc in cur.description]
+    rows = cur.fetchall()
+    return [dict(zip(columns, row)) for row in rows]
 
-# @staticmethod
-# def obj_requis_choix(id_choix):
-#     cur = get_db().cursor()
-#     cur.execute("SELECT O.id_obj, O.nom_obj, R.quantite, R.perd_obj FROM objet O JOIN requis R ON O.id_obj=R.id_obj JOIN choix C ON C.id_choix=R.id_choix WHERE C.id_choix=?", (id_choix,))
-#     columns = [desc[0] for desc in cur.description]
-#     rows = cur.fetchall()
-#     return [dict(zip(columns, row)) for row in rows]
-
-# @staticmethod
-# def obj_obtenus():
-#     cur = get_db().cursor()
-#     cur.execute("SELECT Obj.id_obj, Obj.nom_obj, O.quantite, C.id_chap, C.nom_chap FROM objet Obj JOIN obtenir O ON Obj.id_obj=O.id_obj JOIN chapitre C ON C.id_chap=O.id_chap")
-#     columns = [desc[0] for desc in cur.description]
-#     rows = cur.fetchall()
-#     return [dict(zip(columns, row)) for row in rows]
-
-# @staticmethod
-# def obj_obtenu_chap(id_chap):
-#     cur = get_db().cursor()
-#     cur.execute("SELECT Obj.id_obj, Obj.nom_obj, O.quantite, C.id_chap, C.nom_chap FROM objet Obj JOIN obtenir O ON Obj.id_obj=O.id_obj JOIN chapitre C ON C.id_chap=O.id_chap WHERE C.id_chap=?",(id_chap,))
-#     columns = [desc[0] for desc in cur.description]
-#     rows = cur.fetchall()
-#     return [dict(zip(columns, row)) for row in rows]
+@staticmethod
+def list_items_received(chapter_id):
+    cur = get_db().cursor()
+    cur.execute("SELECT item.id, item.label, receive.quantity, chapter.id_chap, chapter.nom_chap FROM item JOIN receive ON item.id=receive.item_id JOIN chapter ON chapter.id=receive.chapter_id WHERE chapter.id=?",(chapter_id,))
+    columns = [desc[0] for desc in cur.description]
+    rows = cur.fetchall()
+    return [dict(zip(columns, row)) for row in rows]
