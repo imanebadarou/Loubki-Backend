ae34047da4563296c25b8d716c1fde935eb753f3
imanebadarou <54185750+imanebadarou@users.noreply.github.com>
2025-06-24 15:02:50 +0200

    requete sql hist_first_chap

--- api/histoire.py
+++ api/histoire.py
@@ -1,5 +1,5 @@
 from flask_restx import Namespace, Resource, fields
-from model.histoire import list_histoires, histoire_id
+from model.histoire import list_histoires, histoire_id, hist_first_chap
 
 api = Namespace('Histoire', description='Gestion des histoires')
 
@@ -22,3 +22,9 @@ class Histoire(Resource):
     def get(self, key):
         histoireid = histoire_id(key)
         return histoireid
+
+@api.route('/<key>/firstchap', methods=["GET"])
+class Histoire(Resource):
+    def get(self, key):
+        first_chap = hist_first_chap(key)
+        return first_chap
--- model/histoire.py
+++ model/histoire.py
@@ -23,3 +23,11 @@ def histoire_id(id):
 #     columns = [desc[0] for desc in cur.description]
 #     rows = cur.fetchall()
 #     return [dict(zip(columns, row)) for row in rows]
+
+@staticmethod
+def hist_first_chap(id):
+    cur = get_db().cursor()
+    cur.execute('SELECT id_chap, nom_chap, contenu_chap FROM histoire H JOIN chapitre C ON H.first_chap_id=C.id_chap WHERE H.id_hist=?',(id,))
+    columns = [desc[0] for desc in cur.description]
+    rows = cur.fetchall()
+    return [dict(zip(columns, row)) for row in rows]
