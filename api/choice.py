ff10ce5cbbc2d07d6588809597f41e9ee6d9bced
Akingram <guilland.killian@gmail.com>
2025-06-25 21:13:00 +0200

    Added choice CRUD endpoints

--- api/choice.py
+++ api/choice.py
@@ -1,17 +1,51 @@
 from flask_restx import Namespace, Resource, fields
-from model.choice import list_choices, get_choice, list_chapter_choices
+from model.choice import list_choices, get_choice, list_chapter_choices, create_choice, update_choice, delete_choice
 from model.item import list_required_items
 from api.item import item
 
 api = Namespace('Choice', description='Gestion des choix d\'un chapitre')
 
 choice = api.model('Choice', {
-    'id': fields.Integer,
-    'content': fields.String,
-    'prev_chapter_id': fields.Integer,
-    'next_chapter_id': fields.Integer,
+    'id': fields.Integer(readonly=True, description='ID du choix'),
+    'content': fields.String(required=True, description="Description de l'action"),
+    'prev_chapter_id': fields.Integer(required=True, description="ID du chapitre auquel ce choix est rattaché"),
+    'next_chapter_id': fields.Integer(readonly=True, description="ID du chapitre vers lequel ce choix renvoie"),
     'required_items': fields.List(fields.Nested(item)),  # Utilisation de Raw pour les items requis
 })
+
+@api.route('/')
+class ChoiceList(Resource):
+    @api.expect(choice)
+    @api.marshal_with(choice, code=201)
+    def post(self):
+        data = api.payload
+        id = create_choice(data)
+        new_choice = get_choice(id)
+        new_choice['required_items'] = list_required_items(id)
+        # TODO : create linked chapter with default values
+        return new_choice, 201
+
+@api.route('/<int:id>')
+class ChoiceDetail(Resource):
+    @api.marshal_with(choice)
+    def get(self, id):
+        choice = get_choice(id)
+        choice['required_items'] = list_required_items(id)
+        return choice
+    
+    @api.expect(choice)
+    @api.marshal_with(choice)
+    def put(self, id):
+        data = api.payload
+        update_choice(id, data)
+        updated_choice = get_choice(id)
+        updated_choice['required_items'] = list_required_items(id)
+        return updated_choice
+
+    @api.response(204, 'Choice deleted')
+    def delete(self, id):
+        delete_choice(id)
+        # TODO : delete linked chapter
 
 @api.route('/from_story/<int:story_id>', methods=["GET"])
 class ChoiceFromStory(Resource):
@@ -23,14 +57,6 @@ class ChoiceFromStory(Resource):
             choice['required_items'] = list_required_items(choice['id'])
             
         return choices
-    
-@api.route('/<int:id>', methods=["GET"])
-class ChoiceDetail(Resource):
-    @api.marshal_with(choice)
-    def get(self, id):
-        choice = get_choice(id)
-        choice['required_items'] = list_required_items(id)
-        return choice
     
 @api.route('/from_chapter/<int:chapter_id>', methods=["GET"])
 class ChoiceFromChapter(Resource):
--- model/choice.py
+++ model/choice.py
@@ -1,5 +1,7 @@
 from db import get_db
 
+# SELECT
+
 @staticmethod
 def list_choices(story_id):
     cur = get_db().cursor()
@@ -40,3 +42,44 @@ def list_chapter_choices(chapter_id):
     columns = [desc[0] for desc in cur.description]
     rows = cur.fetchall()
     return [dict(zip(columns, row)) for row in rows]
+
+# CREATE
+
+@staticmethod
+def create_choice(data):
+    db = get_db()
+    cur = db.cursor()
+    cur.execute(
+        "INSERT INTO choice (content, prev_chapter_id) VALUES (?, ?)",
+        (data['content'], data['prev_chapter_id'])
+    )
+    db.commit()
+    return cur.lastrowid
+
+@staticmethod
+def update_choice(choice_id, content=None, prev_chapter_id=None):
+    db = get_db()
+    cur = db.cursor()
+    fields = []
+    values = []
+    if content is not None:
+        fields.append("content=?")
+        values.append(content)
+    if prev_chapter_id is not None:
+        fields.append("prev_chapter_id=?")
+        values.append(prev_chapter_id)
+    values.append(choice_id)
+    cur.execute(
+        f"UPDATE choice SET {', '.join(fields)} WHERE id=?",
+        values
+    )
+    db.commit()
+    return cur.rowcount > 0
+
+@staticmethod
+def delete_choice(choice_id):
+    db = get_db()
+    cur = db.cursor()
+    cur.execute("DELETE FROM choice WHERE id=?", (choice_id,))
+    db.commit()
+    return cur.rowcount > 0
