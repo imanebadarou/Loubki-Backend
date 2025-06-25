acaea8f5b795486863a54390aff7ec396330f494
Akingram <guilland.killian@gmail.com>
2025-06-25 20:34:54 +0200

    Added story endpoints

--- model/story.py
+++ model/story.py
@@ -1,5 +1,7 @@
 from db import get_db
 
+# SELECT
+
 @staticmethod
 def list_stories():
     cur = get_db().cursor()
@@ -13,21 +15,43 @@ def get_story(id):
     cur = get_db().cursor()
     cur.execute('SELECT * FROM story WHERE id=?',(id,))
     columns = [desc[0] for desc in cur.description]
-    row = cur.fetchall()
+    row = cur.fetchone()
     return dict(zip(columns, row))
 
-# @staticmethod
-# def histoire_choix(id):
-#     cur = get_db().cursor()
-#     cur.execute('SELECT id_choix, txt_choix, chap_prev, chap_next FROM choix JOIN chapitre ON choix.chap_prev = chapitre.id_chap WHERE chapitre.id_hist=?',(id,))
-#     columns = [desc[0] for desc in cur.description]
-#     rows = cur.fetchall()
-#     return [dict(zip(columns, row)) for row in rows]
+# CREATE
 
-# @staticmethod
-# def hist_first_chap(id):
-#     cur = get_db().cursor()
-#     cur.execute('SELECT id_chap, nom_chap, contenu_chap FROM histoire H JOIN chapitre C ON H.first_chap_id=C.id_chap WHERE H.id_hist=?',(id,))
-#     columns = [desc[0] for desc in cur.description]
-#     rows = cur.fetchall()
-#     return [dict(zip(columns, row)) for row in rows]
+@staticmethod
+def create_story(data):
+    cur = get_db().cursor()
+    cur.execute('INSERT INTO story (name, img_url, description) VALUES (?, ?, ?)',
+                (data['name'], data['img_url'], data['description']))
+    get_db().commit()
+    return
+
+# UPDATE
+
+@staticmethod
+def update_story(id, data):
+    cur = get_db().cursor()
+    fields = []
+    values = []
+    for key in ['name', 'img_url', 'description']:
+        if key in data:
+            fields.append(f"{key}=?")
+            values.append(data[key])
+    if not fields:
+        return  # Nothing to update
+    values.append(id)
+    sql = f"UPDATE story SET {', '.join(fields)} WHERE id=?"
+    cur.execute(sql, values)
+    get_db().commit()
+    return
+
+# DELETE
+
+@staticmethod
+def delete_story(id):
+    cur = get_db().cursor()
+    cur.execute('DELETE FROM story WHERE id=?', (id,))
+    get_db().commit()
+    return
